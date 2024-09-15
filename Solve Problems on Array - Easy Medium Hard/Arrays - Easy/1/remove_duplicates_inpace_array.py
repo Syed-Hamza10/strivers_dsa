2086a756c4869ea905fc6230013edc824d762a56
Syed-Hamza10 <bsdsf21a019@pucit.edu.pk>
2024-09-15 17:14:59 +0500

    nothing

--- Solve Problems on Array - Easy Medium Hard/Arrays - Easy/1/remove_duplicates_inpace_array.py
+++ Solve Problems on Array - Easy Medium Hard/Arrays - Easy/1/remove_duplicates_inpace_array.py
@@ -12,13 +12,19 @@ def brute_force(arr): # NlogN + N
 print(brute_force([1,1,1,2,2,2,3,3,3,4,4,5,5,6,6,7,8]))
 
 
-def optimal(arr): #using Two Pointer Approach, O(N)
+def optimal(arr): #using Two Pointer Approach, O(N), it will only work on sorted arrays
     
     i = 0
 
-    for j in range(len(arr)):
+    for j in range(1, len(arr)):
         if arr[i] != arr[j]:
             arr[i + 1] = arr[j]
             i += 1
     return arr #number of unique elements
-print(optimal([1,1,1,2,2,2,3,3,3,4,4,5,5,6,6,7,8]))
+print(optimal([1,7,1,2,2,2,3,3,3,4,4,5,5,6,6,7,8]))
+
+def list_comprehension(arr):
+    res = []
+    arr = [res.append(x) for x in arr if x not in res]
+    return res
+print(list_comprehension([1,2,3,4,5,5]))
